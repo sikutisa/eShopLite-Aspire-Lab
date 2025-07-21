253dbb4b1918466888508c3e1233439181260aca
sikutisa <ilgooinkorea@gmail.com>
2025-07-21 23:03:49 +0900

    Lab 4

--- Labs/Lab 4 - Data/eShopLite.AppHost/AppHost.cs
+++ Labs/Lab 4 - Data/eShopLite.AppHost/AppHost.cs
@@ -2,13 +2,19 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 var redis = builder.AddRedis("redis");
 
+var productsdb = builder.AddPostgres("pg")
+                            .WithPgAdmin()
+                            .AddDatabase("productsdb");
+
 var products = builder.AddProject<Projects.Products>("products");
 
 builder.AddProject<Projects.Store>("store")
        .WithExternalHttpEndpoints()
        .WithReference(products)
        .WithReference(redis)
+       .WithReference(productsdb)
        .WaitFor(products)
-       .WaitFor(redis);
+       .WaitFor(redis)
+       .WaitFor(productsdb);
  
 builder.Build().Run();
